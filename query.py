d853727d395327ea3e871e70c143278e2144aaaa
charlottedibb <charlotte.dibb@gmail.com>
2022-03-31 23:53:48 -0700

    Test out ergast api

---
+++ query.py
@@ -0,0 +1,10 @@
+import requests
+
+def ergast_retrieve(api_endpoint: str):
+  url = f'https://ergast.com/api/f1/{api_endpoint}.json'
+  response = requests.get(url).json()
+  return response
+
+result = ergast_retrieve('current/driverStandings')
+
+print(result)
