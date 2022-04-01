d362ad45eb1d3e25a39b2a4f96ad108cd0f8a31c
charlottedibb <charlotte.dibb@gmail.com>
2022-03-31 23:59:53 -0700

    Add help message with description

--- query.py
+++ query.py
@@ -1,7 +1,8 @@
 import requests
 import argparse
 
-
+parser = argparse.ArgumentParser(description='Get Formula 1 standings.')
+args = parser.parse_args()
 
 def ergast_retrieve(api_endpoint: str):
   url = f'https://ergast.com/api/f1/{api_endpoint}.json'
