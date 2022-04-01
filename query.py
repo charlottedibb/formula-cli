2fb23009a593c941b5061f75dc7aa1ed1d80636c
charlottedibb <charlotte.dibb@gmail.com>
2022-04-01 00:05:28 -0700

    Get argument from command line and print it back out

--- query.py
+++ query.py
@@ -2,13 +2,16 @@ import requests
 import argparse
 
 parser = argparse.ArgumentParser(description='Get Formula 1 standings.')
+parser.add_argument('request', type=str,
+                    help="Enter the requested info, e.g. driver-standings")
 args = parser.parse_args()
+print(args.request)
+
 
 def ergast_retrieve(api_endpoint: str):
-  url = f'https://ergast.com/api/f1/{api_endpoint}.json'
-  response = requests.get(url).json()
-  return response
+    url = f'https://ergast.com/api/f1/{api_endpoint}.json'
+    response = requests.get(url).json()
+    return response
+
 
 result = ergast_retrieve('current/driverStandings')
-
-print(result)
