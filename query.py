ebc43647f9e6193bb33ff96ac5821eee3b06d318
charlottedibb <charlotte.dibb@gmail.com>
2022-04-01 00:26:22 -0700

    Zero in on important part of json

--- query.py
+++ query.py
@@ -7,15 +7,15 @@ parser = argparse.ArgumentParser(description='Get Formula 1 standings.')
 parser.add_argument('request', type=str,
                     help="Enter the requested info, e.g. driver-standings")
 args = parser.parse_args()
-print(args.request)
 
 
 def ergast_retrieve(api_endpoint: str):
     url = f'https://ergast.com/api/f1/{api_endpoint}.json'
     response = requests.get(url).json()
-    return response
+    return response['MRData']
 
 
 if args.request == 'driver-standings':
     result = ergast_retrieve('current/driverStandings')
-    pprint(result)
+    output = result['StandingsTable']['StandingsLists']
+    pprint(output)
