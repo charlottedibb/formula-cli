68032d5d39931acd1ba38b628602b31acdd342fc
charlottedibb <charlotte.dibb@gmail.com>
2022-04-01 22:36:23 -0700

    Rename variable

--- query.py
+++ query.py
@@ -24,8 +24,8 @@ def ergast_retrieve(api_endpoint: str):
 
 
 if args.request == 'driver-standings':
-    result = ergast_retrieve('current/driverStandings')
-    drivers = result['StandingsTable']['StandingsLists'][0]['DriverStandings']
+    driver_standings = ergast_retrieve('current/driverStandings')
+    drivers = driver_standings['StandingsTable']['StandingsLists'][0]['DriverStandings']
     driver_data = []
     for driver in drivers:
         country_code = get_country_code(driver['Driver']['nationality'])
