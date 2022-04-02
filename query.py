790bf94ddb5e975e917e9e235b29ef31a879f889
charlottedibb <charlotte.dibb@gmail.com>
2022-04-01 22:02:54 -0700

    Create function to lookup country code from nationality

--- query.py
+++ query.py
@@ -1,14 +1,20 @@
 import requests
 import argparse
+import pandas as pd
 import termtables as tt
 import pycountry
+# from countryinfo import CountryInfo
 from pprint import pprint
+
+df = pd.read_csv('./Countries-List.csv')
 
 parser = argparse.ArgumentParser(description='Get Formula 1 standings.')
 parser.add_argument('request', type=str,
                     help="Enter the requested info, e.g. driver-standings")
 args = parser.parse_args()
 
+def get_country_code(nationality: str):
+    return df['ISO 3166 Code'][df['Demonym'] == nationality].values[0]
 
 def ergast_retrieve(api_endpoint: str):
     url = f'https://ergast.com/api/f1/{api_endpoint}.json'
